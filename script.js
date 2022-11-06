45decd889017ca4554a65eabca12be342092114f
@imranahamed <imranahamed733@gmail.com>
2022-11-06 15:17:02 +0600

    typing start in scroll

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 let typeEl = document.querySelectorAll(".type");
 let typeElArr = Array.from(typeEl);
+let enterText = document.querySelector(".enterText");
+let enterSpeed = document.querySelector(".enterSpeed");
+let typebutton = document.querySelector(".typebutton");
 
 
+function typeScript() {
+    typeElArr.map((typeEl) => {
 
+        function isInViewPort() {
+            let viewPort = typeEl.getBoundingClientRect();
 
-typeElArr.map((typeEl) => {
-    let typeText = typeEl.innerHTML;
-    let typeArr;
-    let count = 0;
-    let time = typeEl.dataset.speed;
-    typeEl.innerHTML = "";
+            return (viewPort.top >= 0 && viewPort.left >= 0 && viewPort.bottom <= (window.innerHeight || document.documentElement.clientHeight) && viewPort.right <= (window.innerWidth || document.documentElement.clientWidth));
+        };
 
-    function typejs() {
-        if (count < typeText.length) {
-            typeEl.innerHTML += typeText.charAt(count);
-            count++
-            typeArr = typeText.split("");
-        } else {
-            if (typeArr.length == 0) {
-                count = 0;
+        let typeText = typeEl.innerHTML;
+        let typeArr;
+        let count = 0;
+        let oneTimeRun = true;
+        window.addEventListener("scroll", function () {
+            if (isInViewPort() && oneTimeRun) {
+                oneTimeRun = false;
+                let time = typeEl.dataset.speed;
+                typeEl.innerHTML = "";
+
+                function typejs() {
+                    if (count < typeText.length) {
+                        typeEl.innerHTML += typeText.charAt(count);
+                        count++
+                        typeArr = typeText.split("");
+                    } else {
+                        if (typeArr.length == 0) {
+                            count = 0;
+                        }
+                        typeEl.innerHTML = typeArr.join("")
+                        typeArr.pop()
+                    }
+                }
+
+                setInterval(() => {
+                    typejs()
+                }, time);
             }
-            typeEl.innerHTML = typeArr.join("")
-            typeArr.pop()
-        }
-    }
-
-    setInterval(() => {
-        typejs()
-    }, time);
+        });
+    })
 
 
 
 
 
 
-})
 
 
 
 
+}
+
+
+typeScript()
